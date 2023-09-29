7a6065ee1174e49332e8c10056ed856a1f91ddf3
msHO <115558299+sfs-sh@users.noreply.github.com>
2023-09-29 16:37:24 +0900

    빗방울 내리기

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public GameObject rain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("makeRain", 0, 0.5f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   void makeRain()
+    {
+        Instantiate(rain);
+    }
+}
