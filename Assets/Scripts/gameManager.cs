9156cbeb3a057d7c2b3b4914aa276d53295a788d
msHO <115558299+sfs-sh@users.noreply.github.com>
2023-09-29 17:37:30 +0900

    1차완성

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Security.Cryptography;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject rain;
+    public GameObject panel;
     public static GameManager I;
     public Text scoreText;
+    public Text timeText;
     int totalScore = 0;
+    float limit=6f;
 
     void Awake()
     {
@@ -18,13 +23,20 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         InvokeRepeating("makeRain", 0, 0.5f);
-
+        initGame();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        limit -= Time.deltaTime;
+        if (limit < 0)
+        {           
+            panel.SetActive(true);
+            limit = 0.0f;
+            Time.timeScale = 0.0f;
+        }
+        timeText.text = limit.ToString("N2");
     }
 
     void makeRain()
@@ -38,4 +50,15 @@ public class GameManager : MonoBehaviour
         scoreText.text = totalScore.ToString();
     }
 
+    public void retry()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    void initGame()
+    {
+        Time.timeScale = 1.0f;
+        totalScore = 0;
+        limit = 6f;
+    }
 }
