d223ff00446ac8517e98a44a286eaa80febd92e8
msHO <115558299+sfs-sh@users.noreply.github.com>
2023-09-29 15:12:47 +0900

    first
    Former-commit-id: ff0d17cf7d44a1db1e6ac2020a624a44826a7355

---
+++ Assets/Scripts/rtan.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rtan : MonoBehaviour
+{
+    float direction = 0.1f;
+    float toward = 1.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            toward *= -1;
+            direction *= -1;
+        }
+        if (transform.position.x > 2.8f)
+        {
+            direction = -0.1f;
+            toward = -1.0f;
+        }
+        if (transform.position.x < -2.8f)
+        {
+            direction = 0.1f;
+            toward = 1.0f;
+           
+        }
+
+        transform.localScale = new Vector3(toward, 1, 1);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(direction, 0, 0);
+    }
+}
